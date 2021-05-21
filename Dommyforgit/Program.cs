be3418859a006eb4423d87ca7214f66d127d43c6
Anchal lama <anchallama079@gmail.com>
2021-05-21 10:53:56 +0545

    Add project files.

---
+++ Dommyforgit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dommyforgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+			//int number = 12;
+			bool hungery =true;
+
+			if (hungery ==true)
+			{
+				Console.WriteLine("eat samosa");
+				Console.WriteLine("eat pizza");
+				Console.WriteLine("Burger");
+			
+
+			}
+			
+			else
+			{
+				Console.WriteLine("Do yours  homework");
+			}
+
+		}
+    }
+}
